e6ee79f457b3f4948807b8eaf06f458344f0408c
akshay <akshay@localhost.localdomain>
2018-05-25 15:02:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/About"
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router >
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
